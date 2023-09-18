0b056128a53a6952f6556770b0a511dac2832780
Ammar Mohamadi <webdev@prototyp.fr>
2023-09-18 13:51:42 +0200

    add fee to api routes

--- src/Entity/Fee.php
+++ src/Entity/Fee.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FeeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: FeeRepository::class)]
+#[ApiResource()]
 class Fee
 {
     #[ORM\Id]
